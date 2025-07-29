efb9ec7deb677e2bf6e2d0f17a324a11e745c196
christopher paine <christopher.paine@hotmail.co.uk>
2025-07-29 15:09:15 +0100

    l

---
+++ covariance.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent,md
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# imports
+from IPython.display import display, Markdown, HTML, SVG
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from io import StringIO
+
+
+# %%
+
+
+
+
+
