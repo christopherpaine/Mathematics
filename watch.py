491d75d64d448716c00504baaf20275108f8b888
christopher paine <christopher.paine@hotmail.co.uk>
2025-07-29 19:20:04 +0100

    .

--- watch.py
+++ watch.py
@@ -17,7 +17,7 @@ def watch_directory(path='.'):
             print("Removed: ", ", ".join(removed))
         if modified:
             print("Modified: ", ", ".join(modified))
-            if modified[0]: 
+            if is_python_file(modified[0]): 
                 print ("this was a python file")
                 run_bash_command("jupytext --sync "+str(modified[0]))  
                 run_bash_command("jupyter nbconvert --to notebook --inplace --execute --allow-errors "+remove_file_extension(modified[0])+".ipynb")
@@ -25,13 +25,13 @@ def watch_directory(path='.'):
                 run_bash_command("jupyter nbconvert --to html --no-input --no-prompt " +remove_file_extension(modified[0])+".ipynb" + " --output "+ remove_file_extension(modified[0]) + "-clean.html")
                 print("done  -  in 15 seconds will push to github and take snapshots of timestamps")
                 time.sleep(15)
-                run_bash_command("git add .")
-                run_bash_command("git commit -m '.'")
-                run_bash_command("git push")
-                after = {f: os.path.getmtime(f) for f in os.listdir(path)}
-                added = [f for f in after if f not in before]
-                removed = [f for f in before if f not in after]
-                modified = [f for f in after if f in before and before[f] != after[f]]
+        run_bash_command("git add .")
+        run_bash_command("git commit -m '.'")
+        run_bash_command("git push")
+        after = {f: os.path.getmtime(f) for f in os.listdir(path)}
+        added = [f for f in after if f not in before]
+        removed = [f for f in before if f not in after]
+        modified = [f for f in after if f in before and before[f] != after[f]]
         before = after
         print("waiting...")
 
