8e159fd1c04a0492d70e7a1d0bef629ffebb1580
christopher paine <christopher.paine@hotmail.co.uk>
2025-07-30 10:12:02 +0100

    .

--- eigenvalues_and_eigenvectors.py
+++ eigenvalues_and_eigenvectors.py
@@ -38,72 +38,6 @@ eqn_associativity_of_additions = r"\((\mathbf{u} + \mathbf{v}) + \mathbf{w} = \m
 
 
 
-# %%
-wrd_vector_spaces_intro = "<h2>Vector Spaces</h2>"\
-        + "<p>A vector space is is a set of vectors with two operations:</p>"\
-        + "\
-        <ul>\
-            <li>addition</li>\
-            <li>scalar multiplication</li>\
-        </ul>\
-        "\
-        + "These operations satisfy the following axioms:" + """
-
-<table border="1">
-  <tr>
-    <th style="width: 30%;>Name of Vector Space Axiom</th>
-    <th style="width: 50%;">Explanation</th>
-    <th>Mathematical Notation</th>
-  </tr>
-  <tr>
-    <td>Closure under Addition</td>
-    <td style="width: 50%;">The sum of any two vectors is also a vector in the same space.</td>
-    <td>\(\mathbf{u} + \mathbf{v} \in V\)</td>
-  </tr>
-  <tr>
-    <td>Associativity of Addition</td>
-    <td style="width: 50%;">The way in which vectors are grouped in addition does not change the result.</td>
-    <td>\((\mathbf{u} + \mathbf{v}) + \mathbf{w} = \mathbf{u} + (\mathbf{v} + \mathbf{w})\)</td>
-  </tr>
-  <tr>
-    <td>Identity Element of Addition</td>
-    <td style="width: 50%;">There exists a zero vector such that adding it to any vector does not change the vector.</td>
-    <td>\(\mathbf{v} + \mathbf{0} = \mathbf{v}\)</td>
-  </tr>
-  <tr>
-    <td>Inverse Elements of Addition</td>
-    <td style="width: 50%;">For every vector, there exists an inverse vector such that their sum is the zero vector.</td>
-    <td>\(\mathbf{v} + (-\mathbf{v}) = \mathbf{0}\)</td>
-  </tr>
-  <tr>
-    <td>Closure under Scalar Multiplication</td>
-    <td style="width: 50%;">The product of a scalar and a vector is also a vector in the same space.</td>
-    <td>\(a\mathbf{v} \in V\)</td>
-  </tr>
-  <tr>
-    <td>Distributive Property of Scalar Multiplication with respect to Vector Addition</td>
-    <td style="width: 50%;">Scalar multiplication distributes over vector addition.</td>
-    <td>\(a(\mathbf{u} + \mathbf{v}) = a\mathbf{u} + a\mathbf{v}\)</td>
-  </tr>
-  <tr>
-    <td>Distributive Property of Scalar Multiplication with respect to Field Addition</td>
-    <td style="width: 50%;">Scalar addition distributes over scalar multiplication.</td>
-    <td>\((a + b)\mathbf{v} = a\mathbf{v} + b\mathbf{v}\)</td>
-  </tr>
-  <tr>
-    <td>Associativity of Scalar Multiplication</td>
-    <td style="width: 50%;">The way in which scalars are grouped in multiplication does not change the result.</td>
-    <td>\(a(b\mathbf{v}) = (ab)\mathbf{v}\)</td>
-  </tr>
-  <tr>
-    <td>Identity Element of Scalar Multiplication</td>
-    <td style="width: 50%;">Multiplying a vector by one does not change the vector.</td>
-    <td>\(1\mathbf{v} = \mathbf{v}\)</td>
-  </tr>
-</table>
-"""
-
-
 
 # %%
 wrd_V_vector_space = "We define " + not_V + " as a vector space."
@@ -121,7 +55,6 @@ wrd_aka = "<h2>aka</h2>"+"<p>Eigenvalues are also known as proper values or char
 
 # %%
 display(HTML('<a href="https://christopherpaine.github.io/Mathematics">link home</a>'))
-display(HTML(wrd_vector_spaces_intro))
 display(HTML(wrd_V_vector_space))
 display(HTML(wrd_linear_transformation_intro))
 
