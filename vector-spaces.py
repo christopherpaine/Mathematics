3524529f9c5430cfe231a0812ef484aa53fc6168
christopher paine <christopher.paine@hotmail.co.uk>
2025-07-30 10:21:02 +0100

    .

--- vector-spaces.py
+++ vector-spaces.py
@@ -22,18 +22,6 @@ import pandas as pd
 from io import StringIO
 import notation as ntn
 
-# %%
-not_V = r"\(V\)"
-not_T = r"\(T\)"
-not_transformation = r"\(T: V \rightarrow V\)"
-not_v_exists_in_v = r"\(\mathbf{v} \in V\)"
-not_Tv = r"\(T\mathbf{v}\)"
-not_v = r"\(\mathbf{v}\)"
-not_lamda = r"\(\lambda\)"
-
-
-eqn_tv_lamdav = r"\(T\mathbf{v}=\lambda \mathbf{v}\)"
-eqn_associativity_of_additions = r"\((\mathbf{u} + \mathbf{v}) + \mathbf{w} = \mathbf{u} + (\mathbf{v} + \mathbf{w})\)"
 
 
 
