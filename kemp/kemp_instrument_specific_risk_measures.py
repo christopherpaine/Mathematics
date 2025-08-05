72731c840ea1834427b8321d5c2fee9921662ba7
christopher paine <christopher.paine@hotmail.co.uk>
2025-08-05 13:07:24 +0100

    .

---
+++ kemp/kemp_instrument_specific_risk_measures.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import sys
+import os
+
+
+
+# %%
+sys.path.append(os.path.abspath("/home/chris-jakoolit/Mathematics/_templates"))
+from common_imports import *
+
+
+
+
+
+# %%
+tka.f_css()
+
+
+
+
+
+
+# %%
+display(HTML(fnc.f_md_to_html("./_md/instrument_specific_risk_measures.md")))
